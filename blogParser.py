299887f4d7486bb7a295f536969184025fcdb442
Anusha Kopparam <una.020@gmail.com>
2019-05-09 11:54:14 -0700

    initial commit

---
+++ blogParser.py
@@ -0,0 +1,31 @@
+import glob
+
+count = 0
+text = open("blogs/blogs.txt", "w+")
+gender_text = open("blogs/gender.txt", "w+")
+age_text = open("blogs/age.txt", "w+")
+profession_text = open("blogs/profession.txt", "w+")
+sunsign_text = open("blogs/sunsign.txt", "w+")
+
+
+for filename in glob.glob("blogs/*.xml"):
+	try:
+		file = open(filename, 'rb')
+		# 3804152.female.16.Student.Sagittarius.xml
+		categories = filename.split(".")
+		for line in file.readlines():
+			if line[:1].decode('UTF-8').isalpha():
+				line = line.decode("utf-8")
+				text.write(line)
+				gender_text.write(categories[1]+"\n")
+				age_text.write(categories[2]+"\n")
+				profession_text.write(categories[3]+"\n")
+				sunsign_text.write(categories[4]+"\n")
+	except Exception as e:
+		print(e)
+
+text.close()
+gender_text.close()
+age_text.close()
+profession_text.close()
+sunsign_text.close()
